3e2b62d181c0855c00e69872e6e7c68467b57a7d
Korompai István <korompaii@starschema.net>
2021-10-16 17:16:41 +0200

    clean commit to restart development

---
+++ README.md
@@ -0,0 +1,4 @@
+# Starter template
+
+This is just a simple repo to contain my current favourite React start template.
+
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Very simple starter template that includes React, TypeScript and MaterialUI
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
