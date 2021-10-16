1f5ff6a92a5b5dfc0dc7e9e8e1f82cb731b0a91f
Korompai István <korompaii@starschema.net>
2021-10-16 18:30:22 +0200

    almost working, webpack issue

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,8 @@
 module.exports = function override(config, env) {
+  config.module.rules.push({
+    test: /\.js$/,
+    loader: require.resolve("@open-wc/webpack-import-meta-loader"),
+  });
+
   return config;
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Vizzu from "vizzu";
+import { useRef, useEffect } from "react";
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    let data = {
+      series: [
+        {
+          name: "Foo",
+          values: ["Alice", "Bob", "Ted"],
+          type: "dimension" as const,
+        },
+        { name: "Bar", values: [15, 32, 12], type: "measure" as const },
+        { name: "Baz", values: [5, 3, 2], type: "measure" as const },
+      ],
+    };
+    if (canvasRef.current) {
+      const chart = new Vizzu(canvasRef.current, { data });
+      chart.animate({
+        x: "Foo",
+        y: "Bar",
+      });
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Very simple starter template that includes React, TypeScript and MaterialUI
-        </p>
-      </header>
+    <div>
+      <canvas ref={canvasRef} style={{ width: "100%", height: "100%" }} />
     </div>
   );
 }
