acfe3142f93859bfa483c0af00fffdf36ca4b256
Korompai István <korompaii@starschema.net>
2021-10-18 18:27:46 +0200

    fairly minimal working example

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,37 @@
-module.exports = function override(config, env) {
-  config.module.rules.push({
-    test: /\.js$/,
-    loader: require.resolve("@open-wc/webpack-import-meta-loader"),
-  });
+const path = require("path");
 
-  return config;
+module.exports = {
+  webpack: function override(config, env) {
+    config.module.rules.push({
+      test: /\.js$/,
+      loader: require.resolve("@open-wc/webpack-import-meta-loader"),
+    });
+
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "javascript/auto",
+      loader: "file-loader",
+      options: {
+        // publicPath: "[path][name].[ext]",
+        publicPath: "dist/",
+      },
+    });
+
+    return config;
+  },
+  devServer: function (configFunction) {
+    return function (proxy, allowedHost) {
+      // Create the default config by calling configFunction with the proxy/allowedHost parameters
+      const config = configFunction(proxy, allowedHost);
+      config.contentBase = [
+        config.contentBase,
+        path.join(__dirname, "node_modules/vizzu/dist"),
+      ];
+
+      config.open = false;
+
+      // Return your customised Webpack Development Server config.
+      return config;
+    };
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,127 @@
 import Vizzu from "vizzu";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useReducer } from "react";
+import * as d3 from "d3-fetch";
+import { Data, Config } from "vizzu/dist/vizzu";
+
+function chartDescriptorReducer(
+  state: Config.Chart,
+  action: any
+): Config.Chart {
+  return state;
+}
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  useEffect(() => {
-    let data = {
-      series: [
-        {
-          name: "Foo",
-          values: ["Alice", "Bob", "Ted"],
-          type: "dimension" as const,
-        },
-        { name: "Bar", values: [15, 32, 12], type: "measure" as const },
-        { name: "Baz", values: [5, 3, 2], type: "measure" as const },
-      ],
-    };
-    if (canvasRef.current) {
-      const chart = new Vizzu(canvasRef.current, { data });
-      chart.animate({
-        x: "Foo",
-        y: "Bar",
-      });
+  const chartRef = useRef<Vizzu | null>(null);
+  const [chartConfig, dispatchChartConfig] = useReducer(
+    chartDescriptorReducer,
+    {
+      channels: {
+        x: null,
+        y: null,
+        color: null,
+        lightness: null,
+        size: null,
+        label: null,
+        noop: null,
+      },
+      title: null,
+      legend: null,
+      coordSystem: "cartesian",
+      rotation: 0,
+      geometry: "rectangle",
+      orientation: "horizontal",
+      sort: "none",
+      reverse: false,
+      align: "none",
+      split: false,
     }
+  );
+
+  useEffect(() => {
+    // load the data first
+    d3.csv(process.env.PUBLIC_URL + "superstore.csv").then((data) => {
+      const metaData = {
+        numbers: ["Sales", "Quantity", "Discount", "Profit"],
+      };
+      let vizzuData = {
+        series: Object.keys(data[0]).map((col) => {
+          return {
+            name: col,
+            values: data.map((record) => {
+              let value: number | string = record[col]!;
+              if (metaData.numbers.includes(col)) {
+                value = +value as number;
+              }
+              return value;
+            }),
+          };
+        }),
+      };
+
+      if (!canvasRef.current) {
+        console.error("There is no canvas to draw on! (yet?)");
+      }
+
+      if (chartRef.current) {
+        console.log(
+          "Vizzu already initialized. You probably hot-reloaded the page."
+        );
+        return;
+      }
+
+      chartRef.current = new Vizzu(canvasRef.current!, {
+        data: vizzuData as Data.Set,
+      });
+
+      chartRef.current.animate({
+        x: "Category",
+        y: "Sales",
+      });
+    });
   }, []);
+
+  const gridStyle = {
+    display: "grid",
+    gridTemplateColumns: "160px repeat(9, 1fr)",
+    gridTemplateRows: "repeat(14, 1fr)",
+    height: "100vh",
+  };
+
   return (
-    <div>
-      <canvas ref={canvasRef} style={{ width: "100%", height: "100%" }} />
+    <div className="main" style={gridStyle}>
+      {Object.keys(chartConfig.channels!).map((variableName, idx) => {
+        return (
+          <div
+            style={{ gridRow: `${1 + idx * 2} / span 2`, gridColumn: "1 / 1" }}
+            key={variableName}
+          >
+            {variableName}
+          </div>
+        );
+      })}
+
+      {Object.keys(chartConfig)
+        .filter((k) => k !== "channels")
+        .map((setting, idx) => {
+          return (
+            <div
+              style={{ gridRow: `1/1`, gridColumn: `${2 + idx} / span 1` }}
+              key={setting}
+            >
+              {setting}
+            </div>
+          );
+        })}
+      <canvas
+        ref={canvasRef}
+        style={{
+          width: "100%",
+          height: "100%",
+          gridRow: "2 / span 13",
+          gridColumn: "2 / span 9",
+        }}
+      />
     </div>
   );
 }
